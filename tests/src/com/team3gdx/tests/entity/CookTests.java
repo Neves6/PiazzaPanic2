8cc652777d49518383b1b052dedf130d658b29a6
Herma <49307226+hermaplusplus@users.noreply.github.com>
2023-05-03 02:22:37 +0100

    Test code import optimisation

--- tests/src/com/team3gdx/tests/entity/CookTests.java
+++ tests/src/com/team3gdx/tests/entity/CookTests.java
@@ -7,7 +7,8 @@ import com.team3gdx.tests.GdxTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(GdxTestRunner.class)
 public class CookTests {
